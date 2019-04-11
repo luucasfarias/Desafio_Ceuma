9d4056ed999284a94ff5abda25845c5ddf238c78
lucas farias <luucasfarias21@gmail.com>
2019-04-11 15:04:37 -0300

    creating event monitoring

---
+++ ceuma_api/src/main/java/com/ceuma/api/event/listener/ResourceCreatedListener.java
@@ -0,0 +1,31 @@
+package com.ceuma.api.event.listener;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.ceuma.api.event.ResourceCreatedEvent;
+
+@Component
+public class ResourceCreatedListener implements ApplicationListener<ResourceCreatedEvent> {
+
+	@Override
+	public void onApplicationEvent(ResourceCreatedEvent resourceCreatedEvent) {
+		HttpServletResponse response = resourceCreatedEvent.getResponse();
+		Long id = resourceCreatedEvent.getId();
+		
+		addingHeaderLocation(response, id);
+		
+	}
+
+	private void addingHeaderLocation(HttpServletResponse response, Long id) {
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(id).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+	}
+
+}
