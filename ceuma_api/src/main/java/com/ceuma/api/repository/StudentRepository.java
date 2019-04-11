90a532d3f73b40d73bf11c488c42a2b5e01b7b84
lucas farias <luucasfarias21@gmail.com>
2019-04-11 17:47:47 -0300

    add extends to StudentRepositoryQuery

--- ceuma_api/src/main/java/com/ceuma/api/repository/StudentRepository.java
+++ ceuma_api/src/main/java/com/ceuma/api/repository/StudentRepository.java
@@ -3,7 +3,9 @@ package com.ceuma.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ceuma.api.model.Student;
+import com.ceuma.api.repository.student.StudentRepositoryQuery;
 
-public interface StudentRepository extends JpaRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long>, StudentRepositoryQuery {
 
+	
 }
