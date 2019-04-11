36a4e529eaf8b3e9db77ab3c0225da94f0e7028a
lucas farias <luucasfarias21@gmail.com>
2019-04-11 15:06:39 -0300

    refactoring the response post in the method post mapping

--- ceuma_api/src/main/java/com/ceuma/api/resource/CourseResource.java
+++ ceuma_api/src/main/java/com/ceuma/api/resource/CourseResource.java
@@ -1,12 +1,12 @@
 package com.ceuma.api.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,8 +18,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ceuma.api.event.ResourceCreatedEvent;
 import com.ceuma.api.model.Course;
 import com.ceuma.api.repository.CourceRepository;
 import com.ceuma.api.service.CourseService;
@@ -32,9 +32,12 @@ public class CourseResource {
 
 	@Autowired
 	private CourceRepository courseRepository;
-	
+
 	@Autowired
 	private CourseService courseService;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Course> listAll() {
@@ -44,26 +47,22 @@ public class CourseResource {
 	@PostMapping
 	public ResponseEntity<Course> createCourse(@Valid @RequestBody Course course, HttpServletResponse response) {
 		Course courseSaved = courseRepository.save(course);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
-				.buildAndExpand(courseSaved.getId()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(courseSaved);
+		publisher.publishEvent(new ResourceCreatedEvent(this, response, courseSaved.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(courseSaved);
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Course> searchById(@PathVariable Long id) {
 		Course course = courseRepository.findOne(id);
 		return course != null ? ResponseEntity.ok(course) : ResponseEntity.notFound().build();
 	}
-	
+
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteCourse(@PathVariable Long id) {
 		courseRepository.delete(id);
 	}
-	
+
 	@PutMapping("/{id}")
 	public ResponseEntity<Course> updateCourse(@PathVariable Long id, @Valid @RequestBody Course course) {
 		Course courseSaved = courseService.update(id, course);
--- ceuma_api/src/main/java/com/ceuma/api/resource/StudentResource.java
+++ ceuma_api/src/main/java/com/ceuma/api/resource/StudentResource.java
@@ -2,11 +2,21 @@ package com.ceuma.api.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ceuma.api.event.ResourceCreatedEvent;
 import com.ceuma.api.model.Student;
 import com.ceuma.api.repository.StudentRepository;
 
@@ -17,8 +27,24 @@ public class StudentResource {
 	@Autowired
 	private StudentRepository studentRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Student> listAll(){
 		return studentRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Student> createStudent(@Valid @RequestBody Student student, HttpServletResponse response) {
+		Student studentSaved= studentRepository.save(student);
+		publisher.publishEvent(new ResourceCreatedEvent(this, response, studentSaved.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(studentSaved);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Student> searchById(@PathVariable Long id) {
+		Student student = studentRepository.findOne(id);
+		return student != null ? ResponseEntity.ok(student) : ResponseEntity.notFound().build();
 	}
 }
