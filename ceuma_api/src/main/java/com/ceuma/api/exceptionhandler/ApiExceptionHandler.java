99fd6d5657d19513d0fbf7facf210016f181ccc2
lucas farias <luucasfarias21@gmail.com>
2019-04-11 02:44:58 -0300

    handling exceptions, empty fields and friendly messages

--- ceuma_api/src/main/java/com/ceuma/api/exceptionhandler/ApiExceptionHandler.java
+++ ceuma_api/src/main/java/com/ceuma/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.ceuma.api.exceptionhandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -7,6 +11,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -24,7 +31,30 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		String messageUser = messageSource.getMessage("message.invalid", null, LocaleContextHolder.getLocale());
 		String messageDev = ex.getCause().toString();
-		return handleExceptionInternal(ex, new Erro(messageUser, messageDev), headers, HttpStatus.BAD_REQUEST, request);
+		
+		List<Erro> errors = Arrays.asList(new Erro(messageUser, messageDev));
+		return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		List<Erro> errors = createListErrors(ex.getBindingResult());
+		return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	private List<Erro> createListErrors(BindingResult bindingResult) {
+		List<Erro> errors = new ArrayList<>();
+		
+		for(FieldError fieldError : bindingResult.getFieldErrors()) {
+			String messageUser = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String messageDev = fieldError.toString();
+			errors.add(new Erro(messageUser, messageDev));
+		}
+		
+		
+		return errors;
 	}
 	
 	public static class Erro {
