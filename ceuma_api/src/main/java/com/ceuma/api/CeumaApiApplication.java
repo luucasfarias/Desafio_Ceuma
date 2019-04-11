5f5a795e76f32d14597cd5a26dd807112bab20ee
lucas farias <luucasfarias21@gmail.com>
2019-04-11 00:07:13 -0300

    configuration project and migrations db with flayway

---
+++ ceuma_api/src/main/java/com/ceuma/api/CeumaApiApplication.java
@@ -0,0 +1,13 @@
+package com.ceuma.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeumaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeumaApiApplication.class, args);
+	}
+
+}
---
+++ ceuma_api/src/test/java/com/ceuma/api/CeumaApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ceuma.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CeumaApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
