e84c2b09e7e7d862c11885ee74e29f0c8b12b481
lucas farias <luucasfarias21@gmail.com>
2019-04-11 00:36:07 -0300

    creating course class

---
+++ ceuma_api/src/main/java/com/ceuma/api/model/Course.java
@@ -0,0 +1,87 @@
+package com.ceuma.api.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "course")
+public class Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	private String name;
+
+	@NotNull
+	@Column(name = "date_registro")
+	private LocalDate dateRegister;
+
+	@NotNull
+	private String workload;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getDateRegister() {
+		return dateRegister;
+	}
+
+	public void setDateRegister(LocalDate dateRegister) {
+		this.dateRegister = dateRegister;
+	}
+
+	public String getWorkload() {
+		return workload;
+	}
+
+	public void setWorkload(String workload) {
+		this.workload = workload;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
