59da234d120a5f945d19783791c651b4716c5022
lucas farias <luucasfarias21@gmail.com>
2019-04-12 09:43:17 -0300

    adding logout - remove token on cookie

---
+++ ceuma_api/src/main/java/com/ceuma/api/resource/TokenResource.java
@@ -0,0 +1,30 @@
+package com.ceuma.api.resource;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+	
+	// Removendo token do cookie - logout
+	
+	@DeleteMapping("/revoke")
+	public void remove(HttpServletRequest req, HttpServletResponse resp) {
+		Cookie cookie = new Cookie("refreshToken", null);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(false);
+		cookie.setPath(req.getContextPath() + "/oauth/token");
+		cookie.setMaxAge(0);
+		
+		resp.addCookie(cookie);
+		resp.setStatus(HttpStatus.NO_CONTENT.value());
+	}
+
+}
