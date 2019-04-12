61d9474f4d94472fb394648f352afcc35c0da9e5
lucas farias <luucasfarias21@gmail.com>
2019-04-12 08:01:59 -0300

    api security configuration files

---
+++ ceuma_api/src/main/java/com/ceuma/api/security/AppUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.ceuma.api.security;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ceuma.api.model.User;
+import com.ceuma.api.repository.UserRepository;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<User> usuarioOptional = userRepository.findByEmail(email);
+		User user = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
+		return new org.springframework.security.core.userdetails.User(email, user.getPasswordUser(),
+				getPermissions(user));
+	}
+
+	private Collection<? extends GrantedAuthority> getPermissions(User user) {
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+		user.getPermissions()
+				.forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getDescription().toUpperCase())));
+		return authorities;
+	}
+
+}
