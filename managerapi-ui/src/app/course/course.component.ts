ac14077d098134e557b343f9a820ff6cfacf4c58
lucas farias <luucasfarias21@gmail.com>
2019-04-13 14:59:01 -0300

    array to test for course

--- managerapi-ui/src/app/course/course.component.ts
+++ managerapi-ui/src/app/course/course.component.ts
@@ -6,9 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent {
+
+  dateValue: Date;
+
   courses = [
     { name: 'Adiministração', date_register: new Date(2019, 20, 10), workload: '120 horas' },
     { name: 'Direito', date_register: new Date(2019, 20, 10), workload: '200 horas' },
     { name: 'Medicina', date_register: new Date(2019, 20, 10), workload: '400 horas' }
   ]
+
+  display: boolean = false;
+
+  showDialog() {
+    this.display = true;
+  }
 }
