14130fcc7eacf4436a596fa363a60a80dc2ed23f
lucas farias <luucasfarias21@gmail.com>
2019-04-15 20:22:27 -0300

    service of security

--- managerapi-ui/src/app/security/auth.service.ts
+++ managerapi-ui/src/app/security/auth.service.ts
@@ -5,6 +5,7 @@ import { JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
+  [x: string]: any;
 
   urlTokenJWT = 'http://localhost:8080/oauth/token';
   jwtPayload: any;
@@ -22,14 +23,66 @@ export class AuthService {
     headers.append('Authorization', 'Basic YW5ndWxhcjpAbmd1bEBy');
     const body = `username=${username_app}&password=${password_app}&grant_type=password`;
 
-    return this.htpp.post(this.urlTokenJWT, body, { headers })
+    return this.htpp.post(this.urlTokenJWT, body, { headers, withCredentials: true })
       .toPromise().then(response => {
         this.storeToken(response.json().access_token);
         console.log(response);
       })
       .catch(response => {
-        console.log(response);
+        if (response.status === 400) {
+          const responseJson = response.json();
+          if (responseJson.error === 'invalid_grant') {
+            return Promise.reject('Usuário ou senha inválidos!');
+          }
+        }
+
+        return Promise.reject(response);
       });
+  }
+
+  cleanAccessToken() {
+    localStorage.removeItem('token');
+    this.jwtPayload = null;
+
+  }
+
+  isAccessTokenInvalid() {
+    const token = localStorage.getItem('token');
+
+    return !token || this.jwtHelper.isTokenExpired(token);
+  }
+
+  getNewAccessToken(): Promise<void> {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    headers.append('Authorization', 'Basic YW5ndWxhcjpAbmd1bEBy');
+    const body = 'grant_type=refresh_token';
+
+    return this.http.post(this.urlTokenJWT, body, { headers, withCredentials: true })
+      .toPromise().then(response => {
+        this.storeToken(response.json().access_token);
+        console.log('TCL: Recebe novo accesse token do refresh: ', response);
+        return Promise.resolve(null);
+      })
+      .catch(response => {
+        console.log('TCL:Erro ao renovar token: ', response);
+        return Promise.resolve(null);
+      })
+
+  }
+
+  manyPermissions(roles) {
+    for (const role of roles) {
+      if (this.isAllowed(role)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  isAllowed(permission: string) {
+    return this.jwtPayload && this.jwtPayload.authorities.includes(permission);
   }
 
   private storeToken(token: string) {
