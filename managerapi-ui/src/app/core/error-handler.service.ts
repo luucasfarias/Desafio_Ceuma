746f7e713d157020bd594abe23e88d559f82ce97
lucas farias <luucasfarias21@gmail.com>
2019-04-15 00:01:59 -0300

    implement condition for message type

--- managerapi-ui/src/app/core/error-handler.service.ts
+++ managerapi-ui/src/app/core/error-handler.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
 import { ToastyService } from 'ng2-toasty';
 
 @Injectable()
@@ -10,9 +11,12 @@ export class ErrorHandlerService {
     let message: string;
     if (typeof errorResponse === 'string') {
       message = errorResponse;
-    } else {
-      message = 'Erro ao realizar este processo. Tente novamente.';
+    } else if (errorResponse instanceof Response && errorResponse.status >= 400 && errorResponse.status <= 499) {
+      message = 'Ocorreu um erro ao nesta solicitação';
       console.log('Error message: ', message);
+    } else {
+      message = 'Erro ao processar serviço remoto. Tente novamente.';
+      console.error('Ocorreu um erro', errorResponse);
     }
     this.toasty.error(message);
   }
