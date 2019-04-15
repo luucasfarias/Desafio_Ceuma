be03b51a22a1794d4b81e52d85932e8b12e31360
lucas farias <luucasfarias21@gmail.com>
2019-04-15 10:05:22 -0300

    adding new routes

--- managerapi-ui/src/app/app.module.ts
+++ managerapi-ui/src/app/app.module.ts
@@ -11,13 +11,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { StudentSearchComponent } from './students/student-search/student-search.component';
 import { StudentFormComponent } from './students/student-form/student-form.component';
 import { CourseComponent } from './courses/course/course.component';
+import { CardComponent } from './core/card/card.component';
+import { PageNotFoundComponent } from './core/page-not-found.component';
 
 const routes: Routes = [
+  { path: '', component: CardComponent, pathMatch: 'full' },
   { path: 'alunos', component: StudentSearchComponent },
   { path: 'alunos/novo', component: StudentFormComponent },
   { path: 'alunos/:id', component: StudentFormComponent },
   { path: 'cursos', component: CourseComponent },
   { path: 'cursos:id', component: CourseComponent },
+  { path: 'pagina-nao-encontrada', component: PageNotFoundComponent },
+  { path: '**', redirectTo: 'pagina-nao-encontrada' }
 ]
 
 @NgModule({
