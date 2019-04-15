d741cef87c06735738d120f41fbaefbb9de98ed3
lucas farias <luucasfarias21@gmail.com>
2019-04-15 13:42:33 -0300

    adding new routes

--- managerapi-ui/src/app/app-rounting.module.ts
+++ managerapi-ui/src/app/app-rounting.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { CardComponent } from './core/card/card.component';
 import { StudentSearchComponent } from './students/student-search/student-search.component';
 import { StudentFormComponent } from './students/student-form/student-form.component';
-import { CourseComponent } from './courses/course/course.component';
 import { PageNotFoundComponent } from './core/page-not-found.component';
+import { CourseSearchComponent } from './courses/course-search/course-search.component';
+import { CourseFormComponent } from './courses/course-form/course-form.component';
 
 const routes: Routes = [
   { path: '', component: CardComponent, pathMatch: 'full' },
   { path: 'alunos', component: StudentSearchComponent },
   { path: 'alunos/novo', component: StudentFormComponent },
   { path: 'alunos/:id', component: StudentFormComponent },
-  { path: 'cursos', component: CourseComponent },
-  { path: 'cursos:id', component: CourseComponent },
+  { path: 'cursos', component: CourseSearchComponent },
+  { path: 'cursos/novo', component: CourseFormComponent },
+  { path: 'cursos/:id', component: CourseFormComponent },
   { path: 'pagina-nao-encontrada', component: PageNotFoundComponent },
   { path: '**', redirectTo: 'pagina-nao-encontrada' }
 ]
