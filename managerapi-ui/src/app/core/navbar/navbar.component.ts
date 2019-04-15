70fcb18aefcc92cf904dea516a850598c74198d0
lucas farias <luucasfarias21@gmail.com>
2019-04-15 20:20:38 -0300

    logout function

--- managerapi-ui/src/app/core/navbar/navbar.component.ts
+++ managerapi-ui/src/app/core/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'app/security/auth.service';
+import { LogoutService } from 'app/security/logout.service';
+import { ErrorHandlerService } from '../error-handler.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(
+    private auth: AuthService,
+    private logoutService: LogoutService,
+    private errorHandler: ErrorHandlerService,
+    private router: Router
+  ) { }
 
+  logout() {
+    this.logoutService.logout().then(() => {
+      this.router.navigate(['/login']);
+    })
+      .catch(error => this.errorHandler.handle(error));
+  }
 }
