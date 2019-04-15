b3e793c60a81399fe23a591556716895c2cfb96e
lucas farias <luucasfarias21@gmail.com>
2019-04-15 20:19:50 -0300

    permission in routers with  canActivate

--- managerapi-ui/src/app/app-rounting.module.ts
+++ managerapi-ui/src/app/app-rounting.module.ts
@@ -7,19 +7,50 @@ import { PageNotFoundComponent } from './core/page-not-found.component';
 import { CourseSearchComponent } from './courses/course-search/course-search.component';
 import { CourseFormComponent } from './courses/course-form/course-form.component';
 import { LoginComponent } from './security/login/login.component';
+import { AuthGuard } from './security/auth.guard';
+import { NotAuthorizedComponent } from './core/not-authorized.component';
 
 const routes: Routes = [
   // { path: '', component: CardComponent, pathMatch: 'full' },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'alunos', component: StudentSearchComponent },
-  { path: 'alunos/novo', component: StudentFormComponent },
-  { path: 'alunos/:id', component: StudentFormComponent },
-  { path: 'cursos', component: CourseSearchComponent },
-  { path: 'cursos/novo', component: CourseFormComponent },
-  { path: 'cursos/:id', component: CourseFormComponent },
+  {
+    path: '', redirectTo: 'login', pathMatch: 'full'
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'home', component: CardComponent
+  },
+  {
+    path: 'alunos', component: StudentSearchComponent, canActivate: [AuthGuard],
+    data: { roles: ['ROLE_SEARCH_STUDENT'] }
+  },
+  {
+    path: 'alunos/novo', component: StudentFormComponent, canActivate: [AuthGuard],
+    data: { roles: ['ROLE_REGISTER_STUDENT'] }
+  },
+  {
+    path: 'alunos/:id', component: StudentFormComponent, canActivate: [AuthGuard],
+    data: { roles: ['ROLE_UPDATE_STUDENT'] }
+  },
+  {
+    path: 'cursos', component: CourseSearchComponent, canActivate: [AuthGuard],
+    data: { roles: ['ROLE_SEARCH_COURSE'] }
+  },
+  {
+    path: 'cursos/novo', component: CourseFormComponent, canActivate: [AuthGuard],
+    data: { roles: ['ROLE_REGISTER_COURSE'] }
+  },
+
+  {
+    path: 'cursos/:id', component: CourseFormComponent, canActivate: [AuthGuard],
+    data: { roles: ['ROLE_UPDATE_COURSE'] }
+  },
+
+  { path: 'nao-autorizado', component: NotAuthorizedComponent },
   { path: 'pagina-nao-encontrada', component: PageNotFoundComponent },
-  { path: '**', redirectTo: 'pagina-nao-encontrada' }
+  { path: '**', redirectTo: 'pagina-nao-encontrada' },
+
 ]
 
 @NgModule({
