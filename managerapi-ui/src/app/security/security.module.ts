214152466be8682790776ec672cafe22eb9ec947
lucas farias <luucasfarias21@gmail.com>
2019-04-15 20:22:12 -0300

    factory to security

--- managerapi-ui/src/app/security/security.module.ts
+++ managerapi-ui/src/app/security/security.module.ts
@@ -3,6 +3,21 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { InputTextModule, ButtonModule } from 'primeng/primeng';
 import { FormsModule } from '@angular/forms';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+import { Http, RequestOptions } from '@angular/http';
+import { CeumaHttp } from './ceuma-http';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { LogoutService } from './logout.service';
+
+export function authHttpServiceFactory(auth: AuthService, http: Http, options: RequestOptions) {
+  const config = new AuthConfig({
+    globalHeaders: [
+      { 'Content-Type': 'application/json' }
+    ]
+  });
+  return new CeumaHttp(auth, config, http, options);
+}
 
 @NgModule({
   imports: [
@@ -12,6 +27,15 @@ import { FormsModule } from '@angular/forms';
     ButtonModule
   ],
   declarations: [LoginComponent],
-  exports: [LoginComponent]
+  exports: [LoginComponent],
+  providers: [
+    {
+      provide: AuthHttp,
+      useFactory: authHttpServiceFactory,
+      deps: [AuthService, Http, RequestOptions]
+    },
+    AuthGuard,
+    LogoutService
+  ]
 })
 export class SecurityModule { }
