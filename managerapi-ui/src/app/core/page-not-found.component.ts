d699d8ae6ad33f3173ebd2db7eb933b919687616
lucas farias <luucasfarias21@gmail.com>
2019-04-16 09:37:54 -0300

    adding link back home

--- managerapi-ui/src/app/core/page-not-found.component.ts
+++ managerapi-ui/src/app/core/page-not-found.component.ts
@@ -4,6 +4,10 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-page-not-found',
   template: `
     <div class="container">
+      <div class="ui-g-12">
+        <a routerLink="/home" class="back">Voltar</a>
+      </div>
+
       <h1 class="text-center">Página não encontrada</h1>
     </div>
   `,
